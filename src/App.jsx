b4d4a70d903a5445ca557521936fcbff86fcd84b
jhonjpg <jonathan050315jj@gmail.com>
2023-03-01 21:57:22 -0400

    The Kath Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import { Navbar } from './Components/Navbar'
+
 import Auth from './Routes/auth'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
